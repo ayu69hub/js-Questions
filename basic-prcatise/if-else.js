1e175531c39e04c2cc925ec14dffe070e251096e
ayu69hub <singh6268ayush@.com>
2026-05-25 14:49:23 +0530

    if else

--- basic-prcatise/if-else.js
+++ basic-prcatise/if-else.js
@@ -48,3 +48,41 @@ switch(day){
         console.log("Sunday")
 
 }
+// Even odd
+
+let num = 69;
+
+if(num%2==0){
+    console.log("Even no")
+}else {
+    console.log("odd num")
+}
+
+
+// which is greater
+num1 = 69;
+num2 = 88;
+
+if(num1>num2){
+    console.log("num1 is preater")
+}
+else if (num2>num1){
+    console.log("num2 is greatter")
+}else{
+    console.log("both are equal")
+}
+
+// electric unit
+unit = 115;
+
+
+if(unit>0 & unit<100){
+    console.log("pay 5/unit")
+}else if(unit>100&unit<200){
+    console.log("pay 7/unit");
+    
+}else{
+    console.log("pay 10/unit");
+    
+}
+
