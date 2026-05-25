ffac5200da0b971a8d3593ec2b49a6041db1889f
ayu69hub <singh6268ayush@.com>
2026-05-25 14:47:08 +0530

    first commit

---
+++ basic-prcatise/challenge1.js
@@ -0,0 +1,47 @@
+const { num } = require("./if-else")
+
+let name = "Ayush Singh"
+let age = 20
+let city = "Gwl"
+let married = false
+
+console.log("Myname is " + name + " and I am " + age + " years old. I live in " + city + ". Am I married? " + married)
+
+console.log(typeof (name))
+
+console.log(typeof (age))
+
+let arr = [1, 2, 3, 4, 5]
+console.log(arr)
+console.log(typeof (arr))
+
+let obj = {
+    name: "Ayush",
+    age: 20,
+    city: "Gwl"
+}
+console.log(obj)
+console.log(typeof (obj)) 
+
+console.log(typeof (null))
+console.log(typeof (undefined))
+
+console.log("2"+2)
+console.log(2+2)
+console.log(2+"2")
+console.log("2"+2+2)
+console.log(2+2+"2")
+
+// ternarry operator
+let marks = 55;
+let result = marks>=33? "pass" : "fail";
+console.log(result)
+if (num > 0) {
+    console("+ve num");
+}
+else if (num < 0) {
+    console.log("-ve");
+} else {
+
+    console.log("num is 0");
+}
---
+++ basic-prcatise/if-else.js
@@ -0,0 +1,50 @@
+let age = 20;
+
+if(age >=18){
+    console.log("cam vote")
+}else{
+    console.log("not adult")
+}
+
+// +ve ,-ve ,0 
+
+export let num = 69;
+
+
+
+if(num>0){
+    console("+ve num");
+}
+else if(num<0){
+    console.log("-ve");
+}else{
+    
+    console.log("num is 0");
+}
+
+// 
+
+let day = "Wednesday";
+switch(day){
+    case "monday":
+        console.log("Monday");
+        break;
+    case "tue":
+        console.log("Tuesday");
+        break;
+    case "Wednesday":
+        console.log("Wednesday");
+        break;
+    case "thrusday":
+        console.log("Thrusday");
+        break;
+    case "Friday":
+        console.log("Friday");
+        break;
+    case "sat":
+        console.log("saturay");
+        break;
+    default:
+        console.log("Sunday")
+
+}
